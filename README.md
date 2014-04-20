3d093ec846cc2e1a8937459ec1e841ec9fb9fc60
Brian Detering <bdeterin@gmail.com>
2014-04-19 22:44:42 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+#NodeMySQL
+
+A lightweight wrapper for the [node-mysql](https://github.com/felixge/node-mysql)
+driver.  Providing, select/insert/update/delete and other convenience functions,
+as well as support for promises.
+
---
+++ gruntfile.js
@@ -0,0 +1,21 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        nodeunit: {
+            all: ['test.js']
+        },
+
+        watch: {
+            scripts: {
+                files: ['test.js'],
+                tasks: ['nodeunit']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-nodeunit');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default', ['nodeunit']);
+    grunt.registerTask('test', ['nodeunit']);
+};
